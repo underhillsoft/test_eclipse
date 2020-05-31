066c01931fab0768ecc7140c2282e5154cd0ad1c
underhillsoft <underhillsoft@googole.com>
2020-05-31 18:30:43 +0900

    2nd Commit

--- test/src/com/underhillsoft/Helloworld.java
+++ test/src/com/underhillsoft/Helloworld.java
@@ -3,6 +3,8 @@ package com.underhillsoft;
 public class Helloworld {
 	public static void main(String args[]){
 		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
 		}
 
 }
